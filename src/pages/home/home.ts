2e3092d00a805d9e2fb75ac5200714c159e7ac62
Kirsty Munro <kirsty.munro@cigna.com>
2019-09-11 20:09:18 +0100

    init

---
+++ src/pages/home/home.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FingerprintAIO } from 'ionic-native'
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  check(){
+    console.log('check');
+    FingerprintAIO.isAvailable().then(result =>{
+      console.log(result);
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  show(){
+    console.log('show');
+    FingerprintAIO.show({
+     clientId: "Fingerprint-Demo"
+ }).then(result => {
+      console.log(result);
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+}
